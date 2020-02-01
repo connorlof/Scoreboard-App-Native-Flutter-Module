46bcf09d80f9ffb81362e64d316796e5a38228de
Connor Loftus <connorlof@gmail.com>
2020-02-01 18:13:28 -0500

    added Flutter module code

---
+++ test_flutter_module/lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(chooseWidget(window.defaultRouteName));
+
+Widget chooseWidget(String route) {
+  switch (route) {
+    // name of the route defined in the host app
+    case 'splashRoute':
+      return MyFlutterView();
+
+    default:
+      return MyFlutterView();
+  }
+}
+
+class MyFlutterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Court Counter',
+              style: TextStyle(
+                color: Colors.orange[800],
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.asset('assets/images/basketball.jpg'),
+          ],
+        ),
+      ),
+    );
+  }
+}
